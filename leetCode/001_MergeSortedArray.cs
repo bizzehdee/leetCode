01a6cbd6f2973c6103e03c2d54f82992c650c317
Darren Horrocks <killallthehumans@gmail.com>
2025-02-24 22:34:33 +0000

    Top Interview 150 - 001

---
+++ leetCode/001_MergeSortedArray.cs
@@ -0,0 +1,31 @@
+﻿namespace leetCode;
+
+public class MergeSortedArray
+{
+    public void MergeDotnet(int[] nums1, int m, int[] nums2, int n)
+    {
+        Array.Copy(nums2, 0, nums1, m, n);
+        Array.Sort(nums1);
+    }
+
+
+    public void MergeMine(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            nums1[m + i] = nums2[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int temp = nums1[m + i];
+            int j = m + i - 1;
+            while (j >= 0 && nums1[j] > temp)
+            {
+                nums1[j + 1] = nums1[j];
+                j--;
+            }
+            nums1[j + 1] = temp;
+        }    
+    }
+}
