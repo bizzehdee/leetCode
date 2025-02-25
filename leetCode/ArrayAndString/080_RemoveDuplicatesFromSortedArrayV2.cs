bcd5cf8f4cd0630b326189c60275404ed44f1bfd
Darren Horrocks <killallthehumans@gmail.com>
2025-02-25 21:35:53 +0000

    #80 - remove duplicates from sorted array v2

--- leetCode/ArrayAndString/080_RemoveDuplicatesFromSortedArrayV2.cs
+++ leetCode/ArrayAndString/080_RemoveDuplicatesFromSortedArrayV2.cs
@@ -7,6 +7,23 @@ public class RemoveDuplicatesFromSortedArrayV2
 {
     public int RemoveDuplicatesFromSortedArrayV2Mine(int[] nums)
     {
-        return -1;
+        if (nums.Length == 0) return 0;
+
+        int k = 0;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[k])
+            {
+                k++;
+                nums[k] = nums[i];
+            }
+            else if (k == 0 || nums[k] != nums[k - 1])
+            {
+                k++;
+                nums[k] = nums[i];
+            }
+        }
+
+        return k + 1;
     }
 }
