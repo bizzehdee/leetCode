326b61575066683abe2865ab2ffd50510850f07a
Darren Horrocks <darren.horrocks@ecoonline.com>
2025-02-25 13:15:08 +0000

    Leetcode #169 - Top Interview 150 - Majority Element

--- leetCode.Tests/005_MajorityElementTests.cs
+++ leetCode.Tests/005_MajorityElementTests.cs
@@ -4,6 +4,38 @@ namespace leetCode.Tests;
 public class MajorityElementTests
 {
     [Test]
+    public void Dotnet_Test1()
+    {
+        var me = new MajorityElement();
+        var retVal = me.MajorityElementDotnet(new int[] { 3, 2, 3 });
+        Assert.That(retVal, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Dotnet_Test2()
+    {
+        var me = new MajorityElement();
+        var retVal = me.MajorityElementDotnet(new int[] { 2, 2, 1, 1, 1, 2, 2 });
+        Assert.That(retVal, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Dotnet_Test3()
+    {
+        var me = new MajorityElement();
+        var retVal = me.MajorityElementDotnet(new int[] { 1 });
+        Assert.That(retVal, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Dotnet_Test4()
+    {
+        var me = new MajorityElement();
+        var retVal = me.MajorityElementDotnet(new int[] { 6,5,5 });
+        Assert.That(retVal, Is.EqualTo(5));
+    }
+
+    [Test]
     public void Mine_Test1()
     {
         var me = new MajorityElement();
@@ -17,5 +49,21 @@ public class MajorityElementTests
         var me = new MajorityElement();
         var retVal = me.MajorityElementMine(new int[] { 2, 2, 1, 1, 1, 2, 2 });
         Assert.That(retVal, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Mine_Test3()
+    {
+        var me = new MajorityElement();
+        var retVal = me.MajorityElementMine(new int[] { 1 });
+        Assert.That(retVal, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Mine_Test4()
+    {
+        var me = new MajorityElement();
+        var retVal = me.MajorityElementMine(new int[] { 6, 5, 5 });
+        Assert.That(retVal, Is.EqualTo(5));
     }
 }
--- leetCode/005_MajorityElement.cs
+++ leetCode/005_MajorityElement.cs
@@ -5,8 +5,35 @@
 /// </summary>
 public class MajorityElement
 {
+    public int MajorityElementDotnet(int[] nums)
+    {
+        var k = nums.GroupBy(f => f).OrderByDescending(nums => nums.Count()).First().Key;
+        return k;
+    }
+
     public int MajorityElementMine(int[] nums)
     {
-        return -1;
+        var keyValuePair = new Dictionary<int, int>();
+        var k = 0;
+        var kv = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (keyValuePair.ContainsKey(nums[i]))
+            {
+                keyValuePair[nums[i]]++;
+            }
+            else
+            {
+                keyValuePair.Add(nums[i], 1);
+            }
+
+            if (kv < keyValuePair[nums[i]])
+            {
+                kv = keyValuePair[nums[i]];
+                k = nums[i];
+            }
+        }
+
+        return k;
     }
 }
