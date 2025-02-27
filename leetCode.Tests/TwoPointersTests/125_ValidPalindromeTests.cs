24f7c998877cf62b661b69c17f7b7ab6e1098298
Darren Horrocks <darren.horrocks@ecoonline.com>
2025-02-27 09:41:01 +0000

    Top Interview 150 - Leetcode #125 - Valid Palindrome

---
+++ leetCode.Tests/TwoPointersTests/125_ValidPalindromeTests.cs
@@ -0,0 +1,40 @@
+﻿using leetCode.TwoPointers;
+
+namespace leetCode.Tests.TwoPointersTests;
+
+public class ValidPalindromeTests
+{
+
+    [Test]
+    public void Mine_Test1()
+    {
+        var vp = new ValidPalindrome();
+        var retVal = vp.IsPalindrome("A man, a plan, a canal: Panama");
+        Assert.That(retVal, Is.True);
+    }
+
+    [Test]
+    public void Mine_Test2()
+    {
+        var vp = new ValidPalindrome();
+        var retVal = vp.IsPalindrome("race a car");
+        Assert.That(retVal, Is.False);
+    }
+
+    [Test]
+    public void Mine_Test3()
+    {
+        var vp = new ValidPalindrome();
+        var retVal = vp.IsPalindrome(" ");
+        Assert.That(retVal, Is.True);
+    }
+
+    [Test]
+    public void Mine_Test4()
+    {
+        var vp = new ValidPalindrome();
+        var retVal = vp.IsPalindrome("0P");
+        Assert.That(retVal, Is.False);
+    }
+}
+
---
+++ leetCode/TwoPointers/125_ValidPalindrome.cs
@@ -0,0 +1,44 @@
+﻿namespace leetCode.TwoPointers;
+
+/// <summary>
+/// https://leetcode.com/problems/valid-palindrome/description/?envType=study-plan-v2&envId=top-interview-150
+/// </summary>
+public class ValidPalindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        var left = 0;
+        var right = s.Length - 1;
+
+        while(left <= right)
+        {
+            // skip non-alphanumeric characters
+            while (!char.IsLetterOrDigit(s[left]) && left < right)
+            {
+                left++;
+            }
+            while (!char.IsLetterOrDigit(s[right]) && right > left)
+            {
+                right--;
+            }
+
+            // qucik tolower
+            var l = (s[left] >= 'a' ? s[left] - 32 : s[left]);
+            var r = (s[right] >= 'a' ? s[right] - 32 : s[right]);
+
+            // the actual check
+            if (l != r)
+            {
+                return false;
+            }
+
+            // NEXT!
+            left++;
+            right--;
+        }
+
+        // it didnt fail, so it must be correct?
+        return true;
+    }
+}
+
